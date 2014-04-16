3eaafdeebdbcc25db68d7a98c3c9544a8c95a79c
Sam McLeod <samm@infoxchange.net.au>
2014-04-16 16:34:24 +1000

    betterbash

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+
+GetDockerID() {
+    grep -E '(^|\s)$1($|\s)'
+}
+
+GetDockerID() {
+    grep -E '(^|\s)$1($|\s)'
+}
 
 echo "This will STOP all the following containers, REMOVE them, then DELETE the image"
 
 echo "Containers:"
-docker ps -a | grep -E '(^|\s)$1($|\s)'
+docker ps -a | GetDockerID
 echo "Images:"
-docker images | grep -E '(^|\s)$1($|\s)'
+docker images | GetDockerID
 
 read -r -p "Are you sure? [y/N] " response
 case $response in
     [yY][eE][sS]|[yY])
         echo "There's no going back now..."
-        docker stop $(docker ps -a | grep -E '(^|\s)$1($|\s)' | awk '{print $1}')
-        docker rm $(docker ps -a | grep -E '(^|\s)$1($|\s)' | awk '{print $1}')
-        docker rmi $(docker images | grep -E '(^|\s)$1($|\s)' | awk '{print $3}')
+        docker stop $(docker ps -a | GetDockerID | awk '{print $1}')
+        docker rm $(docker ps -a | GetDockerID | awk '{print $1}')
+        docker rmi $(docker images | GetDockerID | awk '{print $3}')
         ;;
     *)
         exit 0
