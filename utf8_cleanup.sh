2c3fd3f5bc79082e745d46c87b350dc7b0ef7a4d
alexizmailov <alex.izmailov.lin@gmail.com>
2016-02-29 12:36:28 +1100

    There is no spoon... I mean there is no file

--- utf8_cleanup.sh
+++ utf8_cleanup.sh
@@ -5,7 +5,7 @@
 file $1
 
 # Remove bad carridge returns and binary rubbish
-tr -cd '\11\12\15\40-\176' < $1 > "$1"_no_binary.sql
+tr -cd '\11\12\15\40-\176' < $1 > "$1"_file_no_binary.sql
 
 # Make sure it's a stanndard file format
 dos2unix "$1"_file_no_binary.sql
