76bf8e615999fe89325821c767122db9dff3e19c
Sam McLeod <sammcj@users.noreply.github.com>
2022-07-19 16:07:51 +1000

    update scripts

--- postgresql/enable_pg_cron.sql
+++ postgresql/enable_pg_cron.sql
@@ -1,5 +1,5 @@
 -- Enable and setup pg_cron to perform daily vacuums.
--- Must be run against the postgres database - not the application database.
+-- Must be run against the postgres database as the postgres user.
 -- $Q$ behaves the same $$ resulting in a single quote see https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-DOLLAR-QUOTING
 
 DO $Q$ BEGIN
@@ -7,15 +7,15 @@ DO $Q$ BEGIN
       CREATE EXTENSION IF NOT EXISTS pg_cron;
       GRANT USAGE ON SCHEMA cron TO postgres;
 
-      SELECT
+      PERFORM
         cron.schedule(
           'Daily Vacuum', -- name of the cron job
           '15 09 * * *', -- 9:15AM UTC == 7:15PM AEST every day
-        $$ VACUUM FREEZE ANALYZE kis_fsu_ms $$
+        $$ VACUUM FREEZE ANALYZE kis_product_sync $$
         );
 
       -- Job to clean up old job logs
-      SELECT
+      PERFORM
         cron.schedule(
           'Cleanup pg_cron logs',
           '15 09 * * *',
@@ -23,4 +23,4 @@ DO $Q$ BEGIN
           );
 
   END IF;
-END $Q$;
+END $Q$ LANGUAGE plpgsql;
