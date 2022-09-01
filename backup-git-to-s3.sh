bb3e6d7bd302142faab928e0f88a9ba6461d24ed
Sam McLeod <sammcj@users.noreply.github.com>
2022-09-01 15:08:04 +1000

    add s3 git backup script

--- backup-git-to-s3.sh
+++ backup-git-to-s3.sh
@@ -14,6 +14,7 @@ REPOSITORY=${REPOSITORY:=$(printf '%s\n' "${PWD##*/}")}
 REPOSITORY_PATH=${REPOSITORY_PATH:="./"}
 TEMP_DIR="${TEMP_DIR:=/tmp}"
 TAR_COMMAND="${TAR_COMMAND:=tar -czvf}"
+S3_REGION=${S3_REGION:="ap-southeast-2"}
 
 DATE=$(date '+%Y%m%d%H%M%S')
 FILENAME="${REPOSITORY}-${DATE}.tar.gz"
@@ -27,6 +28,7 @@ usage() {
   echo "Environment Variables:"
   echo "  BUCKET: S3 bucket name"
   echo "  PREFIX: S3 prefix (default: githubRepoBackup)"
+  echo "  S3_REGION: S3 region (default: ap-southeast-2)"
   echo "  REPOSITORY: Repository name (default: current directory name)"
   echo "  REPOSITORY_PATH: Repository path (default: ./)"
   echo "  TEMP_DIR: Temporary directory (default: /tmp)"
@@ -55,10 +57,8 @@ echo "Compressing ${REPOSITORY_PATH} as ${FILEPATH}..."
 $TAR_COMMAND "$FILEPATH" $REPOSITORY_PATH || exit_error "Failed to create tar file"
 
 if [ -f "$FILEPATH" ]; then
-  echo "Backing up ${REPOSITORY} from ${REPOSITORY_PATH} as ${FILEPATH} to ${S3_URL}"
-  aws s3 cp "$FILEPATH" "$S3_URL"
-else
-  exit_error "Error uploading ${FILEPATH} to $S3_URL"
+  echo "Backing up ${REPOSITORY} from ${REPOSITORY_PATH} as ${FILEPATH} to ${S3_URL} in region ${S3_REGION}"
+  aws s3 cp "$FILEPATH" "$S3_URL" --region "$S3_REGION" || exit_error "Error uploading ${FILEPATH} to ${S3_URL} in region ${S3_REGION}"
 fi
 
 echo "Succesfully backed up ${REPOSITORY}"
