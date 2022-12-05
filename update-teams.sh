9c1f58c0d5be64e1298026aee517d78b213480e1
Sam McLeod <sammcj@users.noreply.github.com>
2022-12-05 16:40:26 +1100

    update teams

--- update-teams.sh
+++ update-teams.sh
@@ -3,6 +3,13 @@ set -eoux pipefail
 URL="raw.githubusercontent.com/ItzLevvie/MicrosoftTeams-msinternal/master/defconfig"
 PACKAGE_NAME="MicrosoftTeamsArm64Beta.pkg"
 
+# Detect if teams is already installed with homebrew (microsoft-teams cask), if is, skip to removing the audio driver and exit 0
+if [[ -n "$(brew list --cask | grep microsoft-teams)" ]]; then
+  echo "Microsoft Teams is already installed with homebrew, skipping to removing the audio driver and exiting"
+  sudo rm -rf /Library/Audio/Plug-Ins/HAL/MSTeamsAudioDevice.driver
+  exit 0
+fi
+
 # Backup existing installer and cleanup older versions
 if [ -f ~/Downloads/$PACKAGE_NAME ]; then
   rm -f ~/Downloads/MicrosoftTeamsArm64Beta-old.pkg
@@ -16,5 +23,3 @@ curl -L -s $URL | grep "osx-arm64" | head -n 1 | grep -o 'https://staticsint.tea
 sudo /usr/sbin/installer -pkg ~/Downloads/$PACKAGE_NAME -target /Applications/
 
 open /Applications/Microsoft\ Teams.app
-
-sudo rm -rf /Library/Audio/Plug-Ins/HAL/MSTeamsAudioDevice.driver
